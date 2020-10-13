2901c3c572519e148be0396d8d722e1620f49b52
David Ka <david.karnel@student.kyh.se>
2020-10-13 11:59:03 +0200

    ,

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // pop up JOptionPane för sökordet.
+        // ordet (String equals) till inputfilen (hemsida?)
+        // Skriv ut pop-up(?) resultatet.
+
+    }
+}
